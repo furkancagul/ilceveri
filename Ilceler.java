763e09557b5940d896ab327ca79e6cef0a42b59e
furkancagul <94933933+furkancagul@users.noreply.github.com>
2022-01-28 23:55:57 +0300

    Add files via upload

---
+++ Ilceler.java
@@ -0,0 +1,56 @@
+
+package com.final;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Ilceler {
+
+    @SerializedName("IlceAdi")
+    @Expose
+    private String ilceAdi;
+    @SerializedName("LogoURL")
+    @Expose
+    private String logoURL;
+    @SerializedName("BeledeyiBaskani")
+    @Expose
+    private String beledeyiBaskani;
+    @SerializedName("Durum")
+    @Expose
+    private String durum;
+
+    public String getIlceAdi() {
+        return ilceAdi;
+    }
+
+    public void setIlceAdi(String ilceAdi) {
+        this.ilceAdi = ilceAdi;
+    }
+
+    public String getLogoURL() {
+        return logoURL;
+    }
+
+    public void setLogoURL(String logoURL) {
+        this.logoURL = logoURL;
+    }
+
+    public String getBeledeyiBaskani() {
+        return beledeyiBaskani;
+    }
+
+    public void setBeledeyiBaskani(String beledeyiBaskani) {
+        this.beledeyiBaskani = beledeyiBaskani;
+    }
+
+    public String getDurum() {
+        return durum;
+    }
+
+    public void setDurum(String durum) {
+        this.durum = durum;
+    }
+
+}
